87f00ee3cc6c9db5e9d854e9c6a08947dc164140
Matthias Reumann <matthias.reumann@tum.de>
2026-03-25 08:35:39 +0100

    Setup eval and framework

---
+++ include/q9/q9.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace q9 {
+struct Stage {};
+}; // namespace q9
---
+++ q9-eval-mapping/q9-eval-mapping.cpp
@@ -0,0 +1,6 @@
+#include "mlir/Dialect/QCO/Transforms/Passes.h"
+#include "q9/q9.h"
+
+int main() {
+    return 0;
+}
